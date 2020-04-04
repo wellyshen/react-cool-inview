6db03db841f3fcc79460fa133430a94b34f45e33
Welly Shen <hivoid19@gmail.com>
2020-04-04 19:34:32 +0800

    feat: add type definition

--- src/react-cool-inview.d.ts
+++ src/react-cool-inview.d.ts
@@ -1,3 +1,41 @@
 declare module 'react-cool-inview' {
-  // ...
+  import { RefObject } from 'react';
+
+  interface Event {
+    entry?: IntersectionObserverEntry;
+    unobserve?: () => void;
+  }
+
+  export interface ChangeEvent extends Event {
+    inView?: boolean;
+  }
+
+  export interface CallBack<T = Event> {
+    (event?: T): void;
+  }
+
+  interface Options {
+    ssr?: boolean;
+    root?: HTMLElement;
+    rootMargin?: string;
+    threshold?: number | number[];
+    onChange?: CallBack<ChangeEvent>;
+    onEnter?: CallBack;
+    onLeave?: CallBack;
+  }
+
+  interface Return {
+    readonly inView: boolean;
+    readonly entry: IntersectionObserverEntry | null;
+    readonly isObserve: boolean;
+    readonly observe: () => void;
+    readonly unobserve: () => void;
+  }
+
+  const useInView: (
+    ref: RefObject<HTMLElement>,
+    { ssr, root, rootMargin, threshold, onChange, onEnter, onLeave }?: Options
+  ) => Return;
+
+  export default useInView;
 }
