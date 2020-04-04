1520c221b626f183a40bed91f8bafaccf343e55c
Welly Shen <hivoid19@gmail.com>
2020-04-04 23:43:33 +0800

    test: add testing for useLatest hook

---
+++ src/__tests__/useLatest.ts
@@ -0,0 +1,11 @@
+import { renderHook } from '@testing-library/react-hooks';
+
+import useLatest from '../useLatest';
+
+describe('useLatest', () => {
+  it('should return correctly', () => {
+    const val = 'test';
+    const { result } = renderHook(() => useLatest(val));
+    expect(result.current.current).toBe(val);
+  });
+});
