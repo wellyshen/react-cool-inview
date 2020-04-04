85135064c9b96fedc0657328a121882686972911
Welly Shen <hivoid19@gmail.com>
2020-04-04 23:43:51 +0800

    test: remove testing

--- src/__tests__/useInView.tsx
+++
@@ -1,9 +0,0 @@
-import { renderHook } from '@testing-library/react-hooks';
-
-import useInView from '..';
-
-describe('useInView', () => {
-  it('should ...', () => {
-    renderHook(() => useInView());
-  });
-});
