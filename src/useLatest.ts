773f510f7c40f95f07b1d5ff43c26dd6f69924a0
Welly Shen <hivoid19@gmail.com>
2020-04-04 23:45:33 +0800

    refactor: refine utility hook

---
+++ src/useLatest.ts
@@ -0,0 +1,11 @@
+import { RefObject, useRef, useEffect } from 'react';
+
+export default <T>(val: T): RefObject<T> => {
+  const ref = useRef(val);
+
+  useEffect(() => {
+    ref.current = val;
+  }, [val]);
+
+  return ref;
+};
