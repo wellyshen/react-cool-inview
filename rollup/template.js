9cb3efb28776fb5553f53c7ceedcadee18be68a0
Welly Shen <hivoid19@gmail.com>
2020-04-28 21:39:53 +0800

    chore: change favicon set

--- rollup/template.js
+++ rollup/template.js
@@ -3,13 +3,12 @@ export default ({ files }) => `
   <!DOCTYPE html>
   <html lang="en">
     <head>
-      <meta charset="UTF-8" />
+      <meta charset="utf-8" />
+      <link rel="icon" href="assets/favicon.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-      <link rel="apple-touch-icon" sizes="180x180" href="assets/apple-touch-icon.png">
-      <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon-32x32.png">
-      <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon-16x16.png">
-      <link rel="manifest" href="assets/site.webmanifest">
+      <meta name="theme-color" content="#000000" />
+      <link rel="apple-touch-icon" href="assets/logo192.png" />
+      <link rel="manifest" href="assets/manifest.json" />
       <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
       <link rel="stylesheet" href=${files.css[0].fileName}>
       <title>React Cool Inview</title>
