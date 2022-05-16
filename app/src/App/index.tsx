7599e7b450fff1924b0cabc5653383825ba011d8
Welly Shen <hivoid19@gmail.com>
2022-05-16 20:10:40 +0800

    refactor: use import type for types

--- app/src/App/index.tsx
+++ app/src/App/index.tsx
@@ -1,4 +1,5 @@
-import { FC, useState } from "react";
+import type { FC } from "react";
+import { useState } from "react";
 
 import GitHubCorner from "../GitHubCorner";
 import Slider from "../Slider";
--- src/useLatest.ts
+++ src/useLatest.ts
@@ -1,4 +1,5 @@
-import { RefObject, useRef } from "react";
+import type { RefObject } from "react";
+import { useRef } from "react";
 
 export default <T>(val: T): RefObject<T> => {
   const ref = useRef(val);
